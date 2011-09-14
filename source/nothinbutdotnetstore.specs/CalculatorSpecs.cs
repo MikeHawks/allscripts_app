52353bbe1af80d3a3e6c0526be97f53a05b31157
MikeHawks <mike.hawks@allscripts.com>
2011-09-14 16:16:03 -0400

    Pushing new changes

--- source/nothinbutdotnetstore.specs/CalculatorSpecs.cs
+++ source/nothinbutdotnetstore.specs/CalculatorSpecs.cs
@@ -2,6 +2,7 @@
 using Machine.Specifications;
 using developwithpassion.specifications.rhinomocks;
 using developwithpassion.specifications.extensions;
+using System.Data.Common.DbConnection;
 
 namespace nothinbutdotnetstore.specs
 {
@@ -14,19 +15,25 @@ namespace nothinbutdotnetstore.specs
 
         public class when_adding_two_positive_numbers : concern
         {
+
+            Establish c = () =>
+            {
+                connection = depends.on<System.Data.IDbConnecttion>();
+            };
             Because b = () =>
                 result = sut.add(2, 3);
 
             It should_return_the_sum = () =>
                 result.ShouldEqual(5);
 
+            static Connection connection;
             static int result;
         }
 
         public class when_attempting_to_add_a_negative_number : concern
         {
             Because b = () =>
-                spec.catch_exception(() => sut.add(-2, 3));
+                spec.catch_exception(() => sut.add(2, 3));
 
             It should_throw_an_argument_exception = () =>
                 spec.exception_thrown.ShouldBeAn<ArgumentException>();
--- source/nothinbutdotnetstore/Calculator.cs
+++ source/nothinbutdotnetstore/Calculator.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
+using System.Linq;
 
 namespace nothinbutdotnetstore
 {
@@ -6,16 +8,28 @@ namespace nothinbutdotnetstore
     {
         int add(int first, int second);
     }
-
+    
     public class Calculator : ICalculate
     {
         public int add(int first, int second)
         {
+           
+            ensure_all_are_positive(first, second);
             if (first <=0 || second <=0 )
             {
                 throw new ArgumentException();
             }
             return first + second;
+        }
+
+        void ensure_all_are_positive(params int[] numbers)
+        {
+            if(numbers.All(x => x < 0))
+            {
+                throw new ArgumentException();
+            }
+            
+          
         }
     }
 }
